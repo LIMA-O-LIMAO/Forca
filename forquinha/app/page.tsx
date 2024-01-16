d1113cec882fbe46464f83add9f0ec35f646ba21
LIMA-O-LIMAO <joaovitorlima0411@gmail.com>
2024-01-16 00:44:34 -0300

    Sweet Alert com reiniar e A resposta na tela

--- forquinha/app/page.tsx
+++ forquinha/app/page.tsx
@@ -1,26 +1,28 @@
 'use client'
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation'
+import Swal from 'sweetalert2'
+
 
 export default function Home() {
 
   const router = useRouter();
 
 
-  
+
   const [vida, setVida] = useState(6);
   const [resposta, setResposta] = useState(['']);
   const [verificar, setVerificar] = useState('');
+  const [palavra, setpalavra] = useState ('Clara')
 
-  useEffect(() => {
-    const palavra = 'Clara';
-    const Arrays = palavra.split('');
-    setResposta(Arrays.map(() => '_'));
-  }, []);
 
-  
+    useEffect(() => {
+      const Arrays = palavra.split('');
+      setResposta(Arrays.map(() => '_'));
+    }, []);
+
+
   function Teste() {
-    const palavra = 'Clara';
     const Arrays = palavra.split('');
     let letraEncontrada = false;
 
@@ -36,8 +38,8 @@ export default function Home() {
 
     if (!letraEncontrada) {
       setVida((prevVida) => prevVida > 0 ? prevVida - 1 : 0);
-    
-  
+
+
 
     }
 
@@ -45,26 +47,43 @@ export default function Home() {
   }
 
 
+
+  if (vida === 0) {
+    document.getElementById('resp').innerText = palavra
+
+    Swal.fire({
+      title: 'QUE PENA!',
+      titleText: 'Voce Perdeu, Quem sabe na proxima. A Palavra era:',
+      text: 'A palavra era ' +palavra,
+      icon: 'error',
+      confirmButtonText: 'Reiniciar',
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Ação quando o botão de confirmação for pressionado
+        window.location.reload();
+      }
+      // router.push('/Pages');
+    })
  
-
-  if(vida===0){
-
-    router.push('/Pages');
-  
-
   }
 
   return (
     <main className="flex flex-col h-full w-full">
       <h1 className="flex flex-row justify-center">FORCA</h1>
 
-      <div className="gap-6 flex flex-col w-full h-full justify-center text-center items-center">
+      <p id="resp" >saa</p>
+      <div className="gap-6 flex flex-col w-full h-full justify-center text-center items-center pt-40">
         <p className="Linhas"> {resposta.join(' ')}</p>
 
         <input
           type="text"
           maxLength={1}
-          className="border-black/50 border-b-2 w-56 flex justify-center text-center"
+          className=" 
+          w-56 flex justify-center 
+          text-center text-black
+        
+          "
           placeholder="Digite aqui a letra"
           value={verificar}
           id="vai"
@@ -73,7 +92,7 @@ export default function Home() {
         <p>Vidas restantes: {vida}</p>
         <button onClick={Teste}>Enviar</button>
 
-     
+
       </div>
     </main>
   );
